5a4737e38f8511bda3de6542bb1862af2515c243
madhavidave08 <madhavidave@dexters.co.uk>
2026-05-27 14:35:49 +0100

    solved dice test using reader method

--- dice_games/lib/dice.rb
+++ dice_games/lib/dice.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Dice
+  # frozen_string_literal: true
+    attr_reader :values
+  def roll(number_of_dice = 1)
+      @values = Array.new(number_of_dice) { rand(1..6) }
+    end
 end
